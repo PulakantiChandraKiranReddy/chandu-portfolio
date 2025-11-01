380ae92975c68e2eda9e95530a8bab00f034e934
reddy.pulakanti <reddy.pulakanti@hippostores.com>
2025-11-01 19:36:51 +0530

    linkedin URL --fix

--- src/components/HeaderSocials.jsx
+++ src/components/HeaderSocials.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import { BsLinkedin } from 'react-icons/bs'
-import { FaGithub } from 'react-icons/fa'
-import { RiDiscordLine } from 'react-icons/ri'
-import { FaInstagram } from 'react-icons/fa';
-
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { RiDiscordLine } from "react-icons/ri";
+import { FaInstagram } from "react-icons/fa";
 
 const HeaderSocials = () => {
-    return (
-        <div className='header__socials'>
-            <a href="https://www.linkedin.com/in/chandu-reddy-258b94257/" target="_blank"><BsLinkedin /></a>
-            <a href="https://github.com/PulakantiChandraKiranReddy" target="_blank"><FaGithub /></a>
-            <a href="https://www.instagram.com/chandu.reddy20?igsh=MTVqZzhtcjBqamptNA=="><FaInstagram /></a >
-            <a href="https://discord.gg/kURF5bh9"><RiDiscordLine /></a >
-        </div>
-    )
-}
+  return (
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/chandra-kiran-reddy-258b94257/"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/PulakantiChandraKiranReddy" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://www.instagram.com/chandu.reddy20?igsh=MTVqZzhtcjBqamptNA==">
+        <FaInstagram />
+      </a>
+      <a href="https://discord.gg/kURF5bh9">
+        <RiDiscordLine />
+      </a>
+    </div>
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
